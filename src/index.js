b63dabcb757fa40d47bb8f93ca47d979cf7d50b8
Arinde Eniola <eniola.arinde@andela.co>
2015-06-16 22:01:51 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Famous dependencies
+var DOMElement = require('famous/dom-renderables/DOMElement');
+var FamousEngine = require('famous/core/FamousEngine');
+
+// Boilerplate code to make your life easier
+FamousEngine.init();
+
